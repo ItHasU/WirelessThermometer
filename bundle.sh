2e49caacb9b8326160ad7ff232e414371e562d89
Matthieu Aubry <aubry@cervval.com>
2020-05-06 23:09:06 +0200

    Test actions.

--- bundle.sh
+++ bundle.sh
@@ -1,5 +1,10 @@
 TARGET=./bundle/
 
+cd www
+npm install
+npm run build
+cd ..
+
 python -m platformio run -t build
 python -m platformio run -t buildfs
 
@@ -9,4 +14,3 @@ cp ~/.platformio/packages/framework-arduinoespressif32/tools/partitions/boot_app
 cp .pio/build/lolin_d32/partitions.bin ${TARGET}/
 cp .pio/build/lolin_d32/firmware.bin ${TARGET}/
 cp .pio/build/lolin_d32/spiffs.bin ${TARGET}/
-cp upload.sh ${TARGET}/
